55b71239854ab06c76afccd6e92f0a8ad4312972
steven <stevenle2011@gmail.com>
2017-07-26 15:46:15 +0700

    add force graph

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import {
 	selectAll,
 	select,
 	selection,
-	html} from 'd3-selection';
-import {timeParse} from 'd3-time-format';
-import {scaleLinear,scaleQuantile} from 'd3-scale';
-import {range,extent,max} from 'd3-array';
-import {axisBottom,axisLeft} from 'd3-axis';
+	html,
+	event} from 'd3-selection';
+import {
+	forceSimulation,
+	forceLink,
+	forceManyBody,
+	forceCenter
+} from 'd3-force';
+import {drag} from 'd3-drag';
 import {transition} from 'd3-transition';
-import {format} from 'd3-format';
+
 require('./index.css');
 
 const url='https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json'
@@ -23,13 +27,10 @@ function getData(url){
 	})
 }
 function drawGraph(data){
-	console.log(data)
+	//console.log(data)
 	//variable holding svg attributes
-	const margin ={top:100,bottom:100,left:70,right:50}
 	const width = 950;
 	const height = 550;
-	const innerHeight = height - margin.top - margin.bottom;
-	const innerWidth = width - margin.left - margin.right;
 
 	//creates svg
 	let svg = select('body')
@@ -37,8 +38,67 @@ function drawGraph(data){
 		.attr('width',width)
 		.attr('height',height)
 		.attr('class','graph');
+		
+	console.log(data.nodes[data.links[1].target],
+		data.nodes[data.links[1].source])
 
-	let g = svg.append('g')
-		.attr('transform','translate('
-			+ margin.left + ',' + margin.top + ')');
+	let simulation = forceSimulation()
+		.force('link',forceLink())
+		.force('charge',forceManyBody())
+		.force('center',forceCenter(width/2,height/2))
+
+	//console.log(forceLink().id(function(d){return d.id}))
+
+	let link = svg.append('g')
+		.attr('class','links')
+		.selectAll('line')
+		.data(data.links)
+		.enter().append('line')
+		.attr('stroke','black')
+		.attr('stroke-width', 1)
+
+	let node = svg.append('g')
+		.attr('class','nodes')
+		.selectAll('circle')
+		.data(data.nodes)
+		.enter().append('circle')
+			.attr('r',5)
+			.attr('fill','blue')
+			.call(drag()
+				.on('start',dragstarted)
+				.on('drag',dragged)
+				.on('end',dragended));
+		
+	simulation
+		.nodes(data.nodes)
+		.on('tick',ticked);
+
+	simulation.force('link')
+		.links(data.links)
+		
+	function ticked(){
+		link
+			.attr('x1',function(d){return d.source.x;})
+			.attr('y1',function(d){return d.source.y;})
+			.attr('x2',function(d){return d.target.x;})
+			.attr('y2',function(d){return d.target.y;})
+		node
+			.attr('cx',function(d){return d.x})
+			.attr('cy',function(d){return d.y})
+	}
+	function dragstarted(d){
+		if(!event.active) simulation.alphaTarget(0.3).restart();
+		d.fx = d.x;
+		d.fy = d.y
+	}
+	function dragged(d){
+		d.fx = event.x;
+		d.fy = event.y;
+	}
+	function dragended(d){
+		if(!event.active) simulation.alphaTarget(0);
+		d.fx = null;
+		d.fy = null;
+	}
 }
+
