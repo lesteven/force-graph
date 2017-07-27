41bf71e76a428d5b6b2a74114053abcd75970dc5
steven <stevenle2011@gmail.com>
2017-07-27 14:03:34 +0700

    add build

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function drawGraph(data){
 	console.log(data)
 	//variable holding svg attributes
 	const width = 1250;
-	const height = 750;
+	const height = 850;
 
 	//creates svg
 	let svg = select('body')
@@ -39,10 +39,14 @@ function drawGraph(data){
 		.attr('height',height)
 		.attr('class','graph');
 
+	//insert force graph
 	let simulation = forceSimulation()
 		.force('link',forceLink())
-		.force('charge',forceManyBody())
-		.force('center',forceCenter(width,height))
+		.force('charge',forceManyBody()
+			.strength(-100)
+			.distanceMin(100)
+			.distanceMax(100))
+		.force('center',forceCenter(width/2,height/2))
 
 	//console.log(forceLink().id(function(d){return d.id}))
 
@@ -72,6 +76,7 @@ function drawGraph(data){
 			.on('mouseover',function(d){
 				div.transition()
 					.duration(300)
+					.style("z-index", "1")
 					.style('opacity',.9)
 				div.html(d.country)
 					.style('left',(event.pageX -50)+'px')
@@ -91,16 +96,22 @@ function drawGraph(data){
 	simulation.force('link')
 		.links(data.links)
 		
+	svg.append('text')
+		.attr('class','title')
+		.attr('x','45%')
+		.attr('y','8%')
+		.text('National Contiguity')
+
 	function ticked(){
 		const radius = 4;
 		link
-			.attr('x1',function(d){return d.source.x/2;})
-			.attr('y1',function(d){return d.source.y/2;})
-			.attr('x2',function(d){return d.target.x/2;})
-			.attr('y2',function(d){return d.target.y/2;})
+			.attr('x1',function(d){return d.source.x;})
+			.attr('y1',function(d){return d.source.y;})
+			.attr('x2',function(d){return d.target.x;})
+			.attr('y2',function(d){return d.target.y;})
 		node
-			.style('left', function (d) { return d.x/2 + 'px'; })
-            .style('top', function (d) { return d.y/2 + 'px'; });
+			.style('left', function (d) { return d.x + 'px'; })
+            .style('top', function (d) { return d.y + 'px'; });
 	}
 	function dragstarted(d){
 		if(!event.active) simulation.alphaTarget(0.3).restart();
