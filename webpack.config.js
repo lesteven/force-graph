375bcc3dddc5c3c968281fae46c3d4ad76cbbd6a
steven <stevenle2011@gmail.com>
2017-07-27 14:17:53 +0700

    fix build, add flags

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
     loaders: [ 
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/,
               query:{presets:['es2015','env'] }},
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.png$/,loader: "file-loader" }
     ]
   }, 
   plugins: [HtmlWebpackPluginConfig]
